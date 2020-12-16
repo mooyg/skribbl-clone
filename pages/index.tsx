b39853b82d6460d9e8b1695da6c7e73778fb3375
mooyg <mooybot@gmail.com>
2020-12-16 21:18:29 +0530

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from "../styles/Header";
+import Container from "../styles/Container"
+const App = () => {
+
+    return (
+        <Container>
+            <Header>
+             <i><h2>Skribbl...</h2></i>
+            </Header>
+        </Container>
+    );
+};
+
+export default App;
---
+++ styles/Header.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+const Header = styled.div`
+  display: flex;
+  background-color: #101820FF;
+  color: white;
+  padding: 10px;
+  font-size: large;
+`
+export default Header

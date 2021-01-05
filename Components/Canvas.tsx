d29bbc6123a6668e8f7abe308ee9bb13dbf1eb6a
mooyg <mooybot@gmail.com>
2021-01-05 19:40:38 +0530

    .

--- Components/Canvas.tsx
+++ Components/Canvas.tsx
@@ -1,50 +1,61 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, { useEffect, useRef, useState } from "react";
 import CanvasDraw from "react-canvas-draw";
-import {useStateValue} from "../Context/StateProvider";
+import { useStateValue } from "../Context/StateProvider";
 
 const Canvas = () => {
-    const [{socket, userList}] = useStateValue()
-    const canvasRef = useRef<CanvasDraw | null>(null)
-    const [disabled, setDisabled] = useState<boolean>(false)
-    const [timer, setTimer] = useState<number>(20)
-    useEffect(() => {
-        socket.on('broadcast', (data: any) => {
-            console.log("DRAWING BOARD CHANGED", data)
-            canvasRef.current?.loadSaveData(data, true)
-        })
-    }, [])
-    useEffect(()=>{
-        console.log("TURN OVER")
-        if(socket.id !== userList[0]){
-            setDisabled(true)
-        }else {
-            setDisabled(false)
-        }
-    },[userList])
-    const handleDraw = () => {
-        console.log('HEY')
-        if (!canvasRef.current) return;
-        socket.emit('on-draw', canvasRef.current.getSaveData())
+  const [{ socket, userList }, dispatch] = useStateValue();
+  const canvasRef = useRef<CanvasDraw | null>(null);
+  const [disabled, setDisabled] = useState<boolean>(false);
+  const [timer, setTimer] = useState<number>(20);
+  useEffect(() => {
+    socket.on("broadcast", (data: any) => {
+      console.log("DRAWING BOARD CHANGED", data);
+      canvasRef.current?.loadSaveData(data, true);
+    });
+  }, []);
+  useEffect(() => {
+    console.log("TURN OVER");
+    if (socket.id !== userList[0]) {
+      setDisabled(true);
+    } else {
+      setDisabled(false);
     }
-    useEffect(()=>{
-        if(userList.length <=1 ) return;
-        const countdown = setInterval(()=>{
-            setTimer(current => {
-                if(current === 0 ) {
-                    console.log(userList)
-                    return current + 20
-                }
-                return current - 1
-            })
-        },1000)
-        return () => clearInterval(countdown)
-    },[userList])
-    return (
-        <>
-            <h2>{timer}</h2>
-            {/*TODO: make my own Canvas*/}
-            <CanvasDraw disabled={disabled} brushColor={'#ff5733'} ref={canvasRef} onChange={handleDraw}/>
-        </>
-    )
-}
-export default Canvas
+  }, [userList]);
+  const handleDraw = () => {
+    if (!canvasRef.current) return;
+    socket.emit("on-draw", canvasRef.current.getSaveData());
+  };
+  useEffect(() => {
+    if (userList.length <= 1) return;
+    const countdown = setInterval(() => {
+      setTimer((current) => {
+        if (current === 0) {
+          console.log([...userList]);
+          const copyArray = [...userList];
+          const moveUser = copyArray.splice(0, 1);
+          console.log(moveUser);
+          dispatch({
+            type: "SET_USERLIST",
+            item: copyArray.concat(moveUser),
+          });
+          return current + 20;
+        }
+        return current - 1;
+      });
+    }, 1000);
+    return () => clearInterval(countdown);
+  }, [userList]);
+  return (
+    <>
+      <h2>{timer}</h2>
+      {/*TODO: make my own Canvas*/}
+      <CanvasDraw
+        disabled={disabled}
+        brushColor={"#ff5733"}
+        ref={canvasRef}
+        onChange={handleDraw}
+      />
+    </>
+  );
+};
+export default Canvas;
--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ const App = () => {
       </Header>
       <UserList>
         {userList &&
-          userList.map((item, index) => (
+          userList.map((item:string, index:number) => (
             <>
               <h2>
                 {item}-{index}
